b90643590d03ffb8c85970afb3030d2446ab99fb
kaiyu li <krisfk@gmail.com>
2021-10-02 16:29:51 +0800

    .

--- wp-content/themes/starseed/woocommerce/content-single-product.php
+++ wp-content/themes/starseed/woocommerce/content-single-product.php
@@ -210,13 +210,9 @@ if(get_field('icon_and_text_structure'))
         style="position: absolute;top: <?php echo rand(30,100); ?>px;width: <?php echo rand(150,200); ?>px !important;opacity: 0;right: <?php echo rand(-300,-200); ?>px"
         src="http://64.227.13.14/starseed/wp-content/themes/starseed/assets/images/star<?php echo rand(0,10); ?>.png"
         alt="">
-    <img class="star8 fadein-ele animate__animated animate__fadeIn delay-2"
-        style="position: absolute;bottom: <?php echo rand(220,300); ?>px;left: <?php echo rand(-400,-350); ?>px;width: <?php echo rand(250,300); ?>px !important;opacity: 0;"
-        src="http://64.227.13.14/starseed/wp-content/themes/starseed/assets/images/star<?php echo rand(0,10); ?>.png"
-        alt="">
 
     <img class="star7 fadein-ele animate__animated animate__fadeIn delay-2"
-        style="position: absolute;top: <?php echo rand(500,600); ?>px;width: <?php echo rand(150,200); ?>px !important;opacity: 0;right: <?php echo rand(-300,-200); ?>px"
+        style="position: absolute;top: <?php echo rand(500,600); ?>px;width: <?php echo rand(150,200); ?>px !important;opacity: 0;left: <?php echo rand(-300,-200); ?>px"
         src="http://64.227.13.14/starseed/wp-content/themes/starseed/assets/images/star<?php echo rand(0,10); ?>.png"
         alt="">
 
