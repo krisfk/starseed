212cdf38af83e28d6e4d0f84fe22d379c240912e
kaiyu li <krisfk@gmail.com>
2021-10-02 16:57:05 +0800

    .

--- wp-content/themes/starseed/single-post.php
+++ wp-content/themes/starseed/single-post.php
@@ -98,6 +98,16 @@ if( have_rows('content_sections') )
                 ?>
 <div class="container inner-container mt-4 text-center mb-5">
 
+
+    <img class="left-star star7 fadein-ele animate__animated animate__fadeIn delay-2"
+        style="z-index:-10;position: absolute;top: <?php echo rand(30,100); ?>px;width: <?php echo rand(150,200); ?>px !important;opacity: 0;left: <?php echo rand(-300,-200); ?>px"
+        src="http://64.227.13.14/starseed/wp-content/themes/starseed/assets/images/star<?php echo rand(0,10); ?>.png"
+        alt="">
+
+    <img class="right-star fadein-ele animate__animated animate__fadeIn delay-2"
+        style="z-index:-10;position: absolute;top: <?php echo rand(100,200); ?>px;width: <?php echo rand(150,200); ?>px !important;opacity: 0;right: <?php echo rand(-300,-200); ?>px"
+        src="http://64.227.13.14/starseed/wp-content/themes/starseed/assets/images/star<?php echo rand(0,10); ?>.png"
+        alt="">
     <?php if($subtitle)
 {
     ?>
