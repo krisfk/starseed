44c44135b3ab39cb20ff9d65cff39457437b6ae6
kaiyu li <krisfk@gmail.com>
2021-10-05 01:35:29 +0800

    .

--- wp-content/themes/starseed/page-register.php
+++ wp-content/themes/starseed/page-register.php
@@ -13,15 +13,17 @@ get_header();
 ?>
 
 
-<img class="star7 fadein-ele" style="  width: 200px !important;position: absolute;top: 49px;left: 485px;"
-    src="<?php echo get_template_directory_uri();?>/assets/images/star7.png" alt="">
-<img class="star8 fadein-ele" style="  width: 305px !important;position: absolute;bottom: 20px;left: -140px;"
-    src="<?php echo get_template_directory_uri();?>/assets/images/star8.png" alt="">
-<img class="star9 fadein-ele" style="  width: 370px !important;position: absolute;bottom: -120px;left: 543px;"
-    src="<?php echo get_template_directory_uri();?>/assets/images/star6.png" alt="">
-<img class="star10 fadein-ele" style="  width: 370px !important;position: absolute;bottom: 0;right: -165px;z-index: 0;"
-    src="<?php echo get_template_directory_uri();?>/assets/images/star9.png" alt="">
-
+<div class="container position-relative">
+    <img class="star7 fadein-ele" style="  width: 200px !important;position: absolute;top: 49px;left: 485px;"
+        src="<?php echo get_template_directory_uri();?>/assets/images/star7.png" alt="">
+    <img class="star8 fadein-ele" style="  width: 305px !important;position: absolute;bottom: 20px;left: -140px;"
+        src="<?php echo get_template_directory_uri();?>/assets/images/star8.png" alt="">
+    <img class="star9 fadein-ele" style="  width: 370px !important;position: absolute;bottom: -120px;left: 543px;"
+        src="<?php echo get_template_directory_uri();?>/assets/images/star6.png" alt="">
+    <img class="star10 fadein-ele"
+        style="  width: 370px !important;position: absolute;bottom: 0;right: -165px;z-index: 0;"
+        src="<?php echo get_template_directory_uri();?>/assets/images/star9.png" alt="">
+</div>
 <?php
 /* Start the Loop */
 while ( have_posts() ) :
