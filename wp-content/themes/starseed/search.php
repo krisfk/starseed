ca03417221cda29eedb8c35315fd5fd384589e65
kaiyu li <krisfk@gmail.com>
2021-10-22 04:32:07 +0800

    .

--- wp-content/themes/starseed/search.php
+++ wp-content/themes/starseed/search.php
@@ -42,17 +42,7 @@ get_header();
 
 if ( have_posts() ) {
 	?>
-<header class="page-header alignwide">
-    <h1 class="page-title">
-        <?php
-			printf(
-				/* translators: %s: Search term. */
-				esc_html__( 'Results for "%s"', 'twentytwentyone' ),
-				'<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
-			);
-			?>
-    </h1>
-</header><!-- .page-header -->
+
 
 <div class="search-result-count default-max-width">
     <?php
