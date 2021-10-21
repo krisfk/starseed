254c193fc256dbccd1a5922c2f4c8ecd81880cbf
kaiyu li <krisfk@gmail.com>
2021-10-21 22:36:26 +0800

    .

--- wp-content/themes/starseed/page-interstellar-seed-library.php
+++ wp-content/themes/starseed/page-interstellar-seed-library.php
@@ -92,7 +92,7 @@ $category_img = z_taxonomy_image_url(get_queried_object()->term_id);
                     $query->the_post();
                     echo 1;
                 }
-                
+                echo 1;
                 ?>
                 <a href="#" class="post-block">
                     <img class="w-100" src="http://64.227.13.14/starseed/wp-content/uploads/2021/08/key-topic-2.jpeg"
