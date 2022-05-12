d6579c7c0ca13ef7b0b66b14e06c8557b4ff36e4
kaiyu li <krisfk@gmail.com>
2022-05-12 18:10:11 +0800

    .

--- wp-content/themes/starseed/page-key.php
+++ wp-content/themes/starseed/page-key.php
@@ -243,11 +243,11 @@ wp_login_form();
 
 
         <div class="col-lg-8 col-md-12 col-sm-12 col-12  text-end mobile-align">
-            <div class="login-hide">
-                <?php
+            <!-- <div class="login-hide"> -->
+            <?php
             echo get_field('content_3');
             ?>
-            </div>
+            <!-- </div> -->
             <!-- 99 -->
 
             <div class="text-center mb-5">
